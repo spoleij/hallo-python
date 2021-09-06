2d18a24dc9b14ab12755d548a6567649fc8279b2
Steph Poleij <99033734@mydavinci.nl>
2021-09-06 21:23:51 +0200

    nieuwe commit

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 print ('Hello wereld van…   ')                                 
 
-print ('pppp    y     y   ttttttt   h   h    oooo   nnnn ')
+print('pppp    y     y   ttttttt   h   h    oooo   nnnn ')
 print('p   p    y   y       t      h   h   o    o  n   n')
 print('p   p     y y        t      h   h   o    o  n   n')
 print('pppp       y         t      hhhhh   o    o  n   n')
