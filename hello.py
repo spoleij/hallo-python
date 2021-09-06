b1f9bb8c2a9f3d38488d1d412f509dbbf1d1444a
Steph Poleij <99033734@mydavinci.nl>
2021-09-06 11:49:53 +0200

    Eerste commit!

---
+++ hello.py
@@ -0,0 +1,16 @@
+print ('Hello wereld van…   ')                                 
+
+print ('pppp    y     y   ttttttt   h   h    oooo   nnnn ')
+print('p   p    y   y       t      h   h   o    o  n   n')
+print('p   p     y y        t      h   h   o    o  n   n')
+print('pppp       y         t      hhhhh   o    o  n   n')
+print('p          y         t      h   h   o    o  n   n')
+print('p          y         t      h   h   o    o  n   n') 
+print('p          y         t      h   h    oooo   n   n')         
+    
+    
+    
+    
+    
+    
+# groetjes steph 99033734
